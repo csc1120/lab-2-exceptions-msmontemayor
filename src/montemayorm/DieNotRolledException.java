6331b6140af3f89c37a1356fdafa0f984c4c587a
Michael Seanluc Montemayor <montemayorm@msoe.edu>
2024-09-11 15:28:30 -0500

    DieNotRolledException complete

---
+++ src/montemayorm/DieNotRolledException.java
@@ -0,0 +1,19 @@
+/*
+ * Course: CSC1020
+ * Lab 2 - Exceptions
+ * DieNotRolledException class
+ * Name: Michael Seanluc Montemayor
+ * Last Updated: 9/11/2024
+ */
+package montemayorm;
+/**
+ * DieNotRolledException class
+ */
+public class DieNotRolledException extends RuntimeException {
+    /**
+     * Constructor for DieNotRolledException
+     */
+    public DieNotRolledException() {
+        super();
+    }
+}
